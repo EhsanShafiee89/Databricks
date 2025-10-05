4ed269a335dad2a30e930a8b65ee24b621cfc19e
EhsanShafiee89 <ehsan.shafiee68@gmail.com>
2025-10-05 13:28:40 +0200

    Creat cluster

---
+++ Notebook.py
@@ -0,0 +1,72 @@
+# Databricks notebook source
+# MAGIC  %sh
+# MAGIC  rm -r /dbfs/spark_lab
+# MAGIC  mkdir /dbfs/spark_lab
+# MAGIC  wget -O /dbfs/spark_lab/2019.csv https://raw.githubusercontent.com/MicrosoftLearning/mslearn-databricks/main/data/2019_edited.csv
+# MAGIC  wget -O /dbfs/spark_lab/2020.csv https://raw.githubusercontent.com/MicrosoftLearning/mslearn-databricks/main/data/2020_edited.csv
+# MAGIC  wget -O /dbfs/spark_lab/2021.csv https://raw.githubusercontent.com/MicrosoftLearning/mslearn-databricks/main/data/2021_edited.csv
+# MAGIC  
+
+# COMMAND ----------
+
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+orderSchema = StructType([
+     StructField("SalesOrderNumber", StringType()),
+     StructField("SalesOrderLineNumber", IntegerType()),
+     StructField("OrderDate", DateType()),
+     StructField("CustomerName", StringType()),
+     StructField("Email", StringType()),
+     StructField("Item", StringType()),
+     StructField("Quantity", IntegerType()),
+     StructField("UnitPrice", FloatType()),
+     StructField("Tax", FloatType())
+])
+df = spark.read.load('/spark_lab/*.csv', format='csv', schema=orderSchema)
+display(df.limit(100))
+
+# COMMAND ----------
+
+ from pyspark.sql.functions import col
+ df = df.dropDuplicates()
+ df = df.withColumn('Tax', col('UnitPrice') * 0.08)
+ df = df.withColumn('Tax', col('Tax').cast("float"))
+ display(df.limit(100))
+
+# COMMAND ----------
+
+customers = df['CustomerName', 'Email']
+print(customers.count())
+print(customers.distinct().count())
+display(customers.distinct())
+
+# COMMAND ----------
+
+customers = df.select("CustomerName", "Email").where(df['Item']=='Road-250 Red, 52')
+print(customers.count())
+print(customers.distinct().count())
+display(customers.distinct())
+
+# COMMAND ----------
+
+productSales = df.select("Item", "Quantity").groupBy("Item").sum()
+display(productSales)
+
+# COMMAND ----------
+
+yearlySales = df.select(year("OrderDate").alias("Year")).groupBy("Year").count().orderBy("Year")
+display(yearlySales)
+
+# COMMAND ----------
+
+df.createOrReplaceTempView("salesorders")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC     
+# MAGIC SELECT YEAR(OrderDate) AS OrderYear,
+# MAGIC        SUM((UnitPrice * Quantity) + Tax) AS GrossRevenue
+# MAGIC FROM salesorders
+# MAGIC GROUP BY YEAR(OrderDate)
+# MAGIC ORDER BY OrderYear;
